73e41077bdb3bd01237f79a58b7534a3e1237122
Joel Alejandro <joelalejandro.development@gmail.com>
2021-08-31 10:00:20 -0400

    practica 1, completa

---
+++ practice-1.js
@@ -0,0 +1,17 @@
+
+console.log("------------------ Practice 1 ------------------");
+console.log("1 - Solicita un nombre, la edad y muestra por consola el mensaje.\n Hola ____, tienes ____ años y el año que viene tendrás ____ años.\nRealiza el ejercicio con prompt(mensaje) y haz uso de los template strings");
+
+let nombre = "default"
+let edad = 0 ;
+
+nombre = prompt("Ingrese su nombre: ");
+edad = prompt("ingrese su edad: ");
+
+edad = Number(edad);
+
+console.log(`Hola ${nombre}, tienes ${edad} años, y el año que viene tendras ${edad+1}`);
+
+
+console.log("------------------ End ------------------");
+console.log("------------------ Practice 2 ------------------");
