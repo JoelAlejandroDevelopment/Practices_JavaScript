07b3862abe5648e235329a4737c001f3171e8a86
Joel Alejandro <joelalejandro.development@gmail.com>
2021-08-31 10:34:43 -0400

    fin de la practica

--- practice-2.js
+++ practice-2.js
@@ -2,4 +2,53 @@ console.log("------------------ Practice 2 ------------------");
 
 console.log("Escribe un programa que pueda calcular el área de 3 figuras geométricas, triángulo, rectángulo y círculo. En primer lugar pregunta de qué figura se quiere calcular el área, después solicita los datos que necesites para calcularlo. triángulo = b * h/2 rectángulo = b * h círculo = π * r2 (pi * radio al cuadrado)");
 
+let opciones = ["circulo" , "triangulo" , "rectangulo"];
+
+let respuesta;
+respuesta = prompt("De que figura desea sacar el area.\n1. Circulo.\n2. Triangulo.\n3. Rectangulo.\coloque correctamente el nombre.");
+
+let b;
+let h;
+let r;
+let resultado;
+
+respuesta;
+
+switch(respuesta)
+{
+  case "circulo":
+    r = prompt("Ingrese la medida del radio: ");
+    r = Number(r);
+    
+    resultado = Math.PI * Math.pow(r,2);
+    console.log(resultado);
+    break;
+    
+    case "triangulo":
+      b = prompt("Ingrese la medida de la base: ");
+      b = Number(b);
+      h = prompt("Ingrese la medida de la altura: ");
+      h = Number(h);
+
+      resultado = (b*h)/2;
+      
+      console.log(resultado)
+      break;
+      
+      case "rectangulo":
+        b = prompt("Ingrese la medida de la base: ");
+        b = Number(b);
+        h = prompt("Ingrese la medida de la altura: ");
+        h = Number(h);
+        resultado = (b*h);
+        
+        console.log(resultado)
+        break;
+        
+  default:
+    console.log("El dato ingresado es incorrecto") ;
+  break;
+}
+
+
 console.log("------------------ End ------------------");
