31b850a736d39c7945497f52c96bf8d7f4a5c45b
Joel Alejandro <joelalejandro.development@gmail.com>
2021-09-01 21:07:50 -0400

    inicio de la practica 5

---
+++ practice-5.js
@@ -0,0 +1,5 @@
+console.log("------------------ Practice 4 ------------------");
+
+console.log("Escriba un programa que pida un número entero mayor que cero y calcule su factorial.");
+
+console.log("------------------ End ------------------");
