2b1b084231c7e853edbb616a54c995d0b19b6dd5
Joel Alejandro <joelalejandro.development@gmail.com>
2021-09-01 21:09:15 -0400

    fin de la practica 5

--- practice-5.js
+++ practice-5.js
@@ -2,4 +2,19 @@ console.log("------------------ Practice 4 ------------------");
 
 console.log("Escriba un programa que pida un número entero mayor que cero y calcule su factorial.");
 
+function factorial(x) {
+  let retorno = 1;
+  for (let i = x; i > 0; i--) {
+    retorno = Number(retorno) * Number(i);
+  }
+  return retorno;
+}
+
+let entrada = 0;
+
+entrada = prompt("Ingrese un numero para extraer su factorial");
+entrada = Number(entrada);
+
+console.log(`El factorial de ${entrada} es ${factorial(entrada)}`)
+
 console.log("------------------ End ------------------");
