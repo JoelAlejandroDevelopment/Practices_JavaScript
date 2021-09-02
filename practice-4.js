0e15e3f0f5bcba714a01f0d274469e7534fea9fd
Joel Alejandro <joelalejandro.development@gmail.com>
2021-09-01 20:44:26 -0400

    inicio de la practica 4

---
+++ practice-4.js
@@ -0,0 +1,7 @@
+console.log("------------------ Practice 4 ------------------");
+
+console.log("Escribe un programa que pida un número entero mayor que 1 y que escriba si el número primo o no. Un número primo es aquel que solo es divisible por sí mismo y la unidad");
+
+
+
+console.log("------------------ End ------------------");
