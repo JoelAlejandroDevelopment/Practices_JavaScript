2b4b137ead0fc9b8e0e976d7a8b0175c96b12a28
Joel Alejandro <joelalejandro.development@gmail.com>
2021-09-03 17:29:39 -0400

    inicio de la practica 8

---
+++ practice-8.js
@@ -0,0 +1,6 @@
+console.log("------------------ Practice 8 ------------------");
+
+console.log("Dado un array de letras, solicita un número de DNI y calcula que letra le corresponde. El número no puede ser negativo ni tener más de 8 dígitos. La posición de la letra es el resultado del módulo del número introducido entre 23");
+
+
+console.log("------------------ End ------------------");
