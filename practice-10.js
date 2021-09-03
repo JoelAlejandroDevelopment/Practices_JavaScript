45c4ac61ad95c78657eb1cb212d87f31a6336d48
Joel Alejandro <joelalejandro.development@gmail.com>
2021-09-03 18:40:27 -0400

    inicio de la practica 10

---
+++ practice-10.js
@@ -0,0 +1,38 @@
+console.log("------------------ Practice 9 ------------------");
+
+console.log("Solicitar al usuario una palabra y mostrar por consola el número de consonantes, vocales y longitud de la palabra.");
+
+let lenghtString;
+let numeroConsonante = 0;
+let numeroVocales = 0;
+let entrada;
+
+entrada = prompt("Introduzca una palabra.");
+
+lenghtString = entrada.length;
+lenghtString = Number(lenghtString);
+numeroConsonante = Number(numeroConsonante);
+numeroVocales = Number(numeroVocales);
+
+for(let i = 0; i < lenghtString; i++)
+{
+  switch(entrada[i])
+  {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+      numeroVocales++;
+      break;
+    
+    default:
+      numeroConsonante++;
+    break;
+  }
+}
+
+console.log(`La longitud de la palabra es: ${lenghtString}`)
+console.log(`El numero de vocales es: ${numeroVocales}.\nY el numero de consonantes es: ${numeroConsonante}.`);
+
+console.log("------------------ End ------------------");
